6cff1baa9780dfd57e6b9a4139e6c796bb3f6d48
gbTechh <8kdots@gmail.com>
2026-02-06 13:13:48 -0500

    chore: setup project with husky

---
+++ commitlint.config.ts
@@ -0,0 +1,8 @@
+// commitlint.config.ts
+import type { UserConfig } from "@commitlint/types";
+
+const Configuration: UserConfig = {
+  extends: ["@commitlint/config-conventional"],
+};
+
+export default Configuration;
